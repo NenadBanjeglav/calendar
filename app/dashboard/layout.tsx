4bbbc65475ec718b17a8040a3414e6be7ed2b273
NenadBanjeglav <nenadmobmail@gmail.com>
2024-10-02 23:09:00 +0200

    dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,110 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import React, { ReactNode } from "react";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
+import DashboardLinks from "../components/DashboardLinks";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signOut } from "../lib/auth";
+import { requireUser } from "../lib/hook";
 
-const DashboardLayout = () => {
-  return <div>Hello from dashboard layout</div>;
+const DashboardLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await requireUser();
+
+  return (
+    <>
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <Link href="/" className="flex items-center gap-2">
+                <Image src={Logo} alt="Logo" className="size-8" />
+                <p className="text-xl font-bold">Calendar</p>
+              </Link>
+            </div>
+
+            <div className="flex-1">
+              <nav className="grid items-start px-2 lg:px-4">
+                <DashboardLinks />
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  className="shrink-0 md:hidden"
+                  size="icon"
+                  variant="outline"
+                >
+                  <Menu className="size-5" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="flex flex-col">
+                <nav className="mt-10 grid gap-2">
+                  <DashboardLinks />
+                </nav>
+              </SheetContent>
+            </Sheet>
+            <div className="ml-auto flex items-center gap-x-4">
+              <ThemeToggle />
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="rounded-full"
+                  >
+                    <img
+                      src={session?.user?.image as string}
+                      alt="Profile Image"
+                      width={20}
+                      height={20}
+                      className="size-full rounded-full"
+                    />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/settings">Settings</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <form
+                      className="w-full"
+                      action={async () => {
+                        "use server";
+                        await signOut();
+                      }}
+                    >
+                      <button className="w-full text-left">Log out</button>
+                    </form>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </header>
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            {children}
+          </main>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DashboardLayout;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { requireUser } from "../lib/hook";
 
 const DashboardPage = async () => {
-  const session = await requireUser();
   return (
     <div>
       <h1>Hello from the dashboard</h1>
