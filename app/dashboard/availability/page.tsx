5bf12b2b9e615fb78d5b5726dfb7e2af50d0cec7
NenadBanjeglav <nenadmobmail@gmail.com>
2024-10-04 23:52:50 +0200

    availability page

--- app/dashboard/availability/page.tsx
+++ app/dashboard/availability/page.tsx
@@ -1,13 +1,44 @@
+import { UpdateAvailabilityAction } from "@/app/actions";
+import { SubmitButton } from "@/app/components/SubmitButtons";
+import prisma from "@/app/lib/db";
+import { requireUser } from "@/app/lib/hook";
+import { times } from "@/app/lib/times";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const AvailabilityPage = () => {
+async function getData(userId: string) {
+  const data = await prisma.availability.findMany({
+    where: {
+      userId,
+    },
+  });
+  if (!data) {
+    return notFound();
+  }
+  return data;
+}
+
+const AvailabilityPage = async () => {
+  const session = await requireUser();
+  const data = await getData(session.user?.id as string);
+
   return (
     <Card>
       <CardHeader>
@@ -16,8 +47,57 @@ const AvailabilityPage = () => {
           In this section you can manage your availability
         </CardDescription>
       </CardHeader>
-      <form>
-        <CardContent></CardContent>
+      <form action={UpdateAvailabilityAction}>
+        <CardContent className="flex flex-col gap-y-4">
+          {data.map((item) => (
+            <div
+              key={item.id}
+              className="grid grid-cols-1 items-center gap-4 sm:grid-cols-2 md:grid-cols-3"
+            >
+              <input type="hidden" name={`id-${item.id}`} value={item.id} />
+              <div className="flex items-center gap-x-3">
+                <Switch
+                  defaultChecked={item.isActive}
+                  name={`isActive-${item.id}`}
+                />
+                <p>{item.day}</p>
+              </div>
+
+              <Select defaultValue={item.fromTime} name={`fromTime-${item.id}`}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="From Time" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {times.map((time) => (
+                      <SelectItem value={time.time} key={time.id}>
+                        {time.time}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Select defaultValue={item.tillTime} name={`tillTime-${item.id}`}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Till Time" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {times.map((time) => (
+                      <SelectItem value={time.time} key={time.id}>
+                        {time.time}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          ))}
+        </CardContent>
+        <CardFooter>
+          <SubmitButton text="Save Changes" />
+        </CardFooter>
       </form>
     </Card>
   );
